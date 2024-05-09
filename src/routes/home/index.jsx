079608eb9c7ee09bf8f94ac943085a8700467d5a
Victor02H <victor-silva02@outlook.com>
2024-05-09 13:35:21 -0300

    chamada e marcas

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,3 +1,5 @@
+import Brands from "../../components/brands";
+import CallToAction from "../../components/call-to-action";
 import SearchSection from "../../components/search-section";
 import CarsSlider from "./components/cars-slider";
 import CategoriesSection from "./components/categories-section";
@@ -8,8 +10,8 @@ export default function Home() {
       <SearchSection />
       <CarsSlider />
       <CategoriesSection />
-      
-
+      <CallToAction />
+      <Brands />
     </>
   );
 }
