bf7f146da26658fb750f7f5c5e4a4d48674a9f10
Victor02H <victor-silva02@outlook.com>
2024-05-13 13:45:46 -0300

    slider automatizado

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,3 +1,4 @@
+import { slider_data } from "../../data/slider";
 import HeaderNavbar from "./components/navbar";
 
 export default function Header() {
@@ -8,63 +9,34 @@ export default function Header() {
       <header className="header slider-fade">
         <div className="owl-carousel owl-theme owl-loaded owl-drag">
           <div className="owl-stage-outer">
-            <div className="owl-stage" style={{ transform: 'translate3d(-2686px, 0px, 0px)', transition: 'all 0s ease 0s', width: '9401px' }}>
-              <div className="owl-item" style={{ width: 1343 }}>
-                <div className="item bg-img" data-overlay-dark="5" data-background="img/slider/12.jpg" style={{ backgroundImage: 'url(img/slider/12.jpg)' }}>
+            <div className="owl-stage">
+              {slider_data.map((item) => (
+                <div key={item.id} className="owl-item" style={{ width: 1343 }}>
+                  <div className="item bg-img" data-overlay-dark="5" data-background={item.imagem} style={{ backgroundImage: `url(${item.imagem})` }}>
 
-                  <div className="v-middle caption">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-lg-12 col-md-12 mb-30">
-                          <div className="v-middle">
-                            <h6>* Premium</h6>
-                            <h1>Rental Car</h1>
-                            <h5>Rolls Royce Cullinan <span>$900 <i>/ day</i></span></h5> <a href="#0" className="button-1 mt-15 mb-15">View Details <span className="ti-arrow-top-right"></span></a> <a href="#" data-scroll-nav="1" className="button-2 mt-15 mb-15">Rent Now <span className="ti-arrow-top-right"></span></a>
+                    <div className="v-middle caption">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-lg-12 col-md-12 mb-30">
+                            <div className="v-middle">
+                              <h6>{item.subtitulo}</h6>
+                              <h1>{item.titulo}</h1>
+
+                              <div className="col-12">
+                                <a href={item.link} data-scroll-nav="1" className="button-2 mt-15 mb-15">
+                                  Ver Mais
+
+                                  <span className="ti-arrow-top-right"></span>
+                                </a>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="owl-item" style={{ width: 1343 }}>
-                <div className="item bg-img" data-overlay-dark="5" data-background="img/slider/14.jpg" style={{ backgroundImage: 'url(img/slider/14.jpg)' }}>
-
-                  <div className="v-middle caption">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-lg-12 col-md-12 mb-30">
-                          <div className="v-middle">
-                            <h6>* Premium</h6>
-                            <h1>Rental Car</h1>
-                            <h5>Rolls Royce Cullinan <span>$900 <i>/ day</i></span></h5> <a href="#0" className="button-1 mt-15 mb-15">View Details <span className="ti-arrow-top-right"></span></a> <a href="#" data-scroll-nav="1" className="button-2 mt-15 mb-15">Rent Now <span className="ti-arrow-top-right"></span></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="owl-item" style={{ width: 1343 }}>
-                <div className="item bg-img" data-overlay-dark="5" data-background="img/slider/11.jpg" style={{ backgroundImage: 'url(img/slider/11.jpg)' }}>
-
-                  <div className="v-middle caption">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-lg-12 col-md-12 mb-30">
-                          <div className="v-middle">
-                            <h6>* Premium</h6>
-                            <h1>Rental Car</h1>
-                            <h5>Rolls Royce Cullinan <span>$900 <i>/ day</i></span></h5> <a href="#0" className="button-1 mt-15 mb-15">View Details <span className="ti-arrow-top-right"></span></a> <a href="#" data-scroll-nav="1" className="button-2 mt-15 mb-15">Rent Now <span className="ti-arrow-top-right"></span></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
 
@@ -73,10 +45,10 @@ export default function Header() {
             <button type="button" role="presentation" className="owl-next"><i className="ti-angle-right" aria-hidden="true"></i></button>
           </div>
 
-          <div className="owl-dots">
+          {/* <div className="owl-dots">
             <button role="button" className="owl-dot"><span></span></button>
             <button role="button" className="owl-dot"><span></span></button><button role="button" className="owl-dot"><span></span></button>
-          </div>
+          </div> */}
         </div>
       </header>
     </>
---
+++ src/data/slider/index.js
@@ -0,0 +1,23 @@
+export const slider_data = [
+  {
+    id: 1,
+    imagem: 'img/slider/1.jpg',
+    subtitulo: 'Conforto!',
+    titulo: 'Sedans',
+    link: '/'
+  },
+  {
+    id: 2,
+    imagem: 'img/slider/2.jpg',
+    subtitulo: 'Aventuras!',
+    titulo: 'SUVs',
+    link: '/'
+  },
+  {
+    id: 3,
+    imagem: 'img/slider/3.jpg',
+    subtitulo: 'Experiências!',
+    titulo: 'Luxo',
+    link: '/'
+  }
+];
