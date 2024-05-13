6b80b12c906acf81ae85d18e025b49b1945fee09
Victor02H <victor-silva02@outlook.com>
2024-05-13 17:54:43 -0300

    seção de busca atualizado

--- src/components/search-section/index.jsx
+++ src/components/search-section/index.jsx
@@ -4,8 +4,8 @@ export default function SearchSection() {
       <div className="container">
         <div className="row">
           <div className="col-md-12 text-center mb-15">
-            <div className="section-subtitle">Rent Now</div>
-            <div className="section-title">Book Auto Rental</div>
+            <div className="section-subtitle">Busque, Compare, Dirija!</div>
+            <div className="section-title">Encontre o carro ideal</div>
           </div>
         </div>
 
@@ -13,15 +13,16 @@ export default function SearchSection() {
           <form action="#0" className="form1 brdr clearfix">
             <div className="col2 c3">
               <div className="select1_wrapper">
-                <label>Choose Car Type</label>
+                <label>Tipo de carro</label>
+
                 <div className="select1_inner">
                   <select className="select2 select select2-hidden-accessible" style={{ width: "100%" }} data-select2-id="1" tabindex="-1" aria-hidden="true">
-                    <option value="0" data-select2-id="3">Choose Car Type</option>
-                    <option value="1">All</option>
-                    <option value="2">Luxury Cars</option>
-                    <option value="3">Sport Cars</option>
+                    <option value="0" data-select2-id="3">Tipo de carro</option>
+                    <option value="1">Todos</option>
+                    <option value="2">Luxo</option>
+                    <option value="3">Esportivos</option>
                     <option value="4">SUVs</option>
-                    <option value="5">Convertible</option>
+                    <option value="5">Sedans</option>
                   </select>
 
                   {/* <span className="select2 select2-container select2-container--default" dir="ltr" data-select2-id="2" style={{ width: "100%" }}>
@@ -42,15 +43,15 @@ export default function SearchSection() {
 
             <div className="col2 c4">
               <div className="select1_wrapper">
-                <label>Pick Up Location</label>
+                <label>Local de retirada</label>
 
                 <div className="select1_inner">
                   <select className="select2 select select2-hidden-accessible" style={{ width: "100%" }} data-select2-id="4" tabindex="-1" aria-hidden="true">
-                    <option value="0" data-select2-id="6">Pick Up Location</option>
-                    <option value="1">Dubai</option>
-                    <option value="2">Abu Dhabi</option>
-                    <option value="3">Sharjah</option>
-                    <option value="4">Alain</option>
+                    <option value="0" data-select2-id="6">Local de retirada</option>
+                    <option value="1">Amparo</option>
+                    <option value="2">Mogi Mirim</option>
+                    <option value="3">Campinas</option>
+                    <option value="4">São Paulo</option>
                   </select>
 
                   {/* <span className="select2 select2-container select2-container--default" dir="ltr" data-select2-id="5" style={{ width: "100%" }}>
@@ -71,23 +72,25 @@ export default function SearchSection() {
 
             <div className="col1 c1">
               <div className="input1_wrapper">
-                <label>Pick Up Date</label>
+                <label>Data de retirada</label>
+
                 <div className="input1_inner">
-                  <input type="text" className="form-control input datepicker hasDatepicker" placeholder="Pick Up Date" required="" id="dp1715192780061" />
+                  <input type="text" className="form-control input datepicker hasDatepicker" placeholder="Data de retirada" required="" id="dp1715192780061" />
                 </div>
               </div>
             </div>
 
             <div className="col2 c5">
               <div className="select1_wrapper">
-                <label>Drop Off Location</label>
+                <label>Local de entrega</label>
+
                 <div className="select1_inner">
                   <select className="select2 select select2-hidden-accessible" style={{ width: "100%" }} data-select2-id="7" tabindex="-1" aria-hidden="true">
-                    <option value="0" data-select2-id="9">Drop Off Location</option>
-                    <option value="1">Alain</option>
-                    <option value="2">Sharjah</option>
-                    <option value="3">Abu Dhabi</option>
-                    <option value="4">Dubai</option>
+                    <option value="0" data-select2-id="9">Local de entrega</option>
+                    <option value="1">Amparo</option>
+                    <option value="2">Mogi Mirim</option>
+                    <option value="3">Campinas</option>
+                    <option value="4">São Paulo</option>
                   </select>
 
                   {/* <span className="select2 select2-container select2-container--default" dir="ltr" data-select2-id="8" style={{ width: "100%" }}>
@@ -108,15 +111,16 @@ export default function SearchSection() {
 
             <div className="col1 c2">
               <div className="input1_wrapper">
-                <label>Return Date</label>
+                <label>Data de retorno</label>
+
                 <div className="input1_inner">
-                  <input type="text" className="form-control input datepicker hasDatepicker" placeholder="Return Date" id="dp1715192780062" />
+                  <input type="text" className="form-control input datepicker hasDatepicker" placeholder="Data de retorno" id="dp1715192780062" />
                 </div>
               </div>
             </div>
 
             <div className="col3 c6">
-              <button type="submit" className="booking-button">Rent Now</button>
+              <button type="submit" className="booking-button">Alugar Agora</button>
             </div>
           </form>
         </div>
