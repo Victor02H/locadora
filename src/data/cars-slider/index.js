3bbe1541b9804c7b5fcf8225018ea21a231aff86
Victor02H <victor-silva02@outlook.com>
2024-05-13 17:55:27 -0300

    slider de carros automatizado

---
+++ src/data/cars-slider/index.js
@@ -0,0 +1,20 @@
+export const cars_slider_data = [
+  {
+    id: 1,
+    nome: 'Audi Q3',
+    imagem: 'img/cars/4.jpg',
+    valor: 700.00
+  },
+  {
+    id: 2,
+    nome: 'Honda Civic 2020',
+    imagem: 'img/cars/2.jpg',
+    valor: 500.00
+  },
+  {
+    id: 3,
+    nome: 'VW Polo 2021',
+    imagem: 'img/cars/3.jpg',
+    valor: 250.00
+  },
+];
--- src/routes/home/components/cars-slider/index.jsx
+++ src/routes/home/components/cars-slider/index.jsx
@@ -1,3 +1,6 @@
+import ValueFormatter from "../../../../components/value-formatter";
+import { cars_slider_data } from "../../../../data/cars-slider";
+
 export default function CarsSlider() {
   return (
     <>
@@ -5,64 +8,69 @@ export default function CarsSlider() {
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center mb-30">
-              <div className="section-subtitle">Select Your Car</div>
-              <div className="section-title">Luxury <span>Car Fleet</span></div>
+              <div className="section-subtitle">Explorando Horizontes</div>
+              <div className="section-title">Rumo a <span>Aventura</span></div>
             </div>
           </div>
 
           <div className="cars1-carousel owl-theme owl-carousel owl-loaded owl-drag">
             <div className="owl-stage-outer">
               <div className="owl-stage">
-                <div className="owl-item" style={{ width: "800px", marginRight: "20px" }}>
-                  <div className="item">
-                    <div className="img">
-                      <img src="img/slider/12.jpg" alt="" />
-                    </div>
+                {cars_slider_data.map((item) => (
+                  <div key={item.id} className="owl-item" style={{ width: "800px", marginRight: "20px" }}>
+                    <div className="item">
+                      <div className="img">
+                        <img src={item.imagem} alt={item.nome} />
+                      </div>
 
-                    <div className="con opacity-1">
-                      <div className="row">
-                        <div className="col-md-7">
-                          <div className="title">
-                            <a href="#">Rolls Royce Cullinan</a>
-                          </div>
-
-                          <div className="details">
-                            <span>
-                              <i className="omfi-door"></i> 4 Seats
-                            </span>
-
-                            <span>
-                              <i className="omfi-transmission"></i> Auto
-                            </span>
-
-                            <span>
-                              <i className="omfi-luggage"></i> 4 Bags
-                            </span>
-
-                            <span>
-                              <i className="omfi-age"></i> Age 25
-                            </span>
-                          </div>
-                        </div>
-
-                        <div className="col-md-5">
-                          <div className="book">
-                            <div>
-                              <a href="car-details.html" className="btn">
-                                <span>Details</span>
-                              </a>
+                      <div className="con opacity-1">
+                        <div className="row">
+                          <div className="col-md-7">
+                            <div className="title">
+                              <a href="#">{item.nome}</a>
                             </div>
 
-                            <div>
-                              <span className="price">$900</span>
-                              <span>/day</span>
+                            <div className="details">
+                              <span>
+                                <i className="omfi-door"></i> 2 Lugares
+                              </span>
+
+                              <span>
+                                <i className="omfi-transmission"></i> Automatizado
+                              </span>
+
+                              <span>
+                                <i className="omfi-luggage"></i> 4 Bags
+                              </span>
+
+                              <span>
+                                <i className="omfi-age"></i> Age 25
+                              </span>
+                            </div>
+                          </div>
+
+                          <div className="col-md-5">
+                            <div className="book">
+                              <div>
+                                <a href="car-details.html" className="btn">
+                                  <span>Detalhes</span>
+                                </a>
+                              </div>
+
+                              <div>
+                                <span className="price">
+                                  <ValueFormatter value={item.valor} />
+                                </span>
+
+                                <span>/dia</span>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                ))}
               </div>
             </div>
 
